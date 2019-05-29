43f5eb11c1ad8c926c034422de2022419512b7ca
Charl Cronje <charlcp@gmail.com>
2019-05-29 07:44:45 +0200

    MySQL Database Dump

---
+++ bookings.sql
@@ -0,0 +1,133 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50726
+Source Host           : localhost:3306
+Source Database       : bookings
+
+Target Server Type    : MYSQL
+Target Server Version : 50726
+File Encoding         : 65001
+
+Date: 2019-05-29 07:44:23
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for bookings
+-- ----------------------------
+DROP TABLE IF EXISTS `bookings`;
+CREATE TABLE `bookings` (
+  `id` int(11) NOT NULL,
+  `guest_id` int(11) DEFAULT NULL,
+  `room_id` int(11) DEFAULT NULL,
+  `checkin_dtime` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `checkout_dtime` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `notes` text,
+  `created_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of bookings
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for guests
+-- ----------------------------
+DROP TABLE IF EXISTS `guests`;
+CREATE TABLE `guests` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `initials` varchar(20) DEFAULT NULL,
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `id_number` varchar(13) DEFAULT NULL,
+  `telephone` varchar(15) DEFAULT NULL,
+  `mobile` varchar(15) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of guests
+-- ----------------------------
+INSERT INTO `guests` VALUES ('1', 'cp', 'Charl', 'Cronje', '8302235074087', '0729596396', '0729596396', 'charlcp@gmail.com', '2019-05-29 07:41:47', '2019-05-29 07:41:50');
+
+-- ----------------------------
+-- Table structure for migrations
+-- ----------------------------
+DROP TABLE IF EXISTS `migrations`;
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Records of migrations
+-- ----------------------------
+INSERT INTO `migrations` VALUES ('1', '2014_10_12_000000_create_users_table', '1');
+
+-- ----------------------------
+-- Table structure for password_resets
+-- ----------------------------
+DROP TABLE IF EXISTS `password_resets`;
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Records of password_resets
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for rooms
+-- ----------------------------
+DROP TABLE IF EXISTS `rooms`;
+CREATE TABLE `rooms` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `room_name` varchar(100) DEFAULT NULL,
+  `room_number` varchar(10) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of rooms
+-- ----------------------------
+INSERT INTO `rooms` VALUES ('1', 'Room 1', '1', '2019-05-28 22:53:48', '2019-05-28 22:53:47');
+INSERT INTO `rooms` VALUES ('2', 'Room 2', '2', '2019-05-28 22:53:43', '2019-05-28 22:53:44');
+INSERT INTO `rooms` VALUES ('3', 'Room 3', '3', '2019-05-28 22:53:39', '2019-05-28 22:53:41');
+INSERT INTO `rooms` VALUES ('4', 'Room 4', '4', '2019-05-28 22:53:36', '2019-05-28 22:53:37');
+INSERT INTO `rooms` VALUES ('5', 'Room 5', '5', '2019-05-28 22:53:32', '2019-05-28 22:53:33');
+INSERT INTO `rooms` VALUES ('6', 'Room 6', '6', '2019-05-28 22:53:51', '2019-05-28 22:53:50');
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES ('1', 'charl', 'charlcp@gmail.com', null, '$2y$10$BuyxdqHj9bDMrsC/hvq3NeDa.Wh/biK8zObVV7FGMlxXOZHPtyFhu', 'aC7rbpixqNFcw92XufYpWSDusClc18xpnhZUF46KvePJby1zaBckvukVgh0J', '2019-05-28 11:16:14', '2019-05-28 11:16:14');
